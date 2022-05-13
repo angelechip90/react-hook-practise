bae09d5c7f4782006f3e39a3c79b707c513df18b
bui nguyen <phuong.nguyenthanh90@gmail.com>
2022-05-13 16:50:57 +0700

    change1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import './App.css';
-import ColorBox from './components/ColorBox';
 import ClockFeature from './features/Clock';
 import HeroFeature from './features/Hero';
 import MagicBoxFeature from './features/MagicBox';
@@ -7,12 +9,34 @@ import PostFeatures from './features/PostList/Index';
 import TodoListFeature from './features/TodoList';
 
 function App() {
-
-
   return (
+    <div className="app">
+      <p>Header</p>
 
-    <div className="App">
-      <h1>Clock</h1>
+      <p className='link-list'><Link to="/clock">Clock</Link></p>
+      <p className='link-list'><Link to="/hero">Hero</Link></p>
+      <p className='link-list'><Link to="/magicbox">Magic box</Link></p>
+      <p className='link-list'><Link to="/post">Post List</Link></p>
+      <p className='link-list'><Link to="/todo">Todo List</Link></p>
+      <p>
+
+      </p>
+
+      <p className='link-list'><NavLink to="/clock">Clock</NavLink></p>
+      <p className='link-list'><NavLink to="/hero">Hero</NavLink></p>
+      <p className='link-list'><NavLink to="/magicbox">Magic box</NavLink></p>
+      <p className='link-list'><NavLink to="/post">Post List</NavLink></p>
+      <p className='link-list'><NavLink to="/todo">Todo List</NavLink></p>
+
+      <Route path="/clock" component={ClockFeature} />
+      <Route path="/hero" component={HeroFeature} />
+      <Route path="/magicbox" component={MagicBoxFeature} />
+      <Route path="/post" component={PostFeatures} />
+      <Route path="/todo" component={TodoListFeature} />
+
+      Footer
+
+      {/* <h1>Clock</h1>
       <ClockFeature />
       <h1>To Do List</h1>
       <TodoListFeature />
@@ -21,7 +45,9 @@ function App() {
       <h1>Magic Box</h1>
       <MagicBoxFeature />
       <h1>Hero</h1>
-      <HeroFeature />
+      <HeroFeature /> */}
+
+
       {/* <ColorBox />
       <TodoListFeature /> */}
 
