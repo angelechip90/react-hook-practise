d68aaf120483a564e262792546300d8bcf25b46d
bui nguyen <phuong.nguyenthanh90@gmail.com>
2022-05-13 15:54:58 +0700

    finish

--- src/features/Hero/index.jsx
+++ src/features/Hero/index.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useMemo, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 import Hero from './components/Hero';
 
@@ -9,13 +9,25 @@ function HeroFeature(props) {
     const [count, setCount] = useState(0);
 
 
-
-
     //useMemo => dùng cho giá trị và trả về giá trị cũ(mảng, ...) khi re-render
     //const data = useMemo(() => [{}, {}, {}], []);
 
     //usecallback => dùng cho function và trả về hàm cũ khi re-render
     const handleHeroClick = useCallback(() => { }, []);
+
+    //useRef: cách giữ giá trị trước đó của 1 state
+    //áp dụng: ví dụ cái vị trí bản đồ, search vị trí 1 xong search vị trí 2
+
+    const [countnum, setCountnum] = useState(0);
+    const prevCount = useRef(countnum);
+
+    useEffect(() => {
+        prevCount.current = countnum;
+    }, [countnum]);
+
+    const handleIncreaseClick = () => {
+        setCountnum(x => x + 1);
+    }
 
     //hạn chế dùng 2 ông này, chỉ dùng khi thấy project chạy khá nặng và cần tối ưu
 
@@ -25,7 +37,11 @@ function HeroFeature(props) {
             <button onClick={() => setCount(count + 1)}>Increase</button>
 
             <Hero name="Supermen" onClick={handleHeroClick} />
-        </div >
+
+            <p>Previous: {prevCount.current}</p>
+            <p>Current: {countnum}</p>
+            <button onClick={handleIncreaseClick}>Tăng lên</button>
+        </div>
     );
 }
 
